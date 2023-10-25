3dda0c5d2dd3c68dc48a45cb6d75c47ea8b39d25
aastha-cse <guptaaastha824@gmail.com>
2023-10-25 11:31:54 +0530

    Added passing percentage to quiz model

--- backend/src/models/quiz.ts
+++ backend/src/models/quiz.ts
@@ -17,6 +17,10 @@ const quizSchema = new schema(
       },
     ],
     answers: {},
+    passing_percentage: {
+      type: Number,
+      required: true
+    },
     createdBy: {
       type: mongoose.Types.ObjectId,
       required: true,
