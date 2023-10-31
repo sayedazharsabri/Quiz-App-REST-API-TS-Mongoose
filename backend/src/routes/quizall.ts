f1830e104f51821d83c292fdbf3d552756a1b379
syeda <syeda@Raziuddins-MacBook-Pro.local>
2023-10-31 11:00:16 +0100

    Added user authentication middleware to the route

--- backend/src/routes/quizall.ts
+++ backend/src/routes/quizall.ts
@@ -1,11 +1,12 @@
 import express from 'express';
-import { getallQuiz } from '../controllers/quizall';
 
+import { getallQuiz } from '../controllers/quizall';
+import { isAuthenticated } from '../middlewares/isAuth';
 
 const router = express.Router();
 
 //Get All published Quiz
-router.get("/",  getallQuiz );
+router.get("/", isAuthenticated, getallQuiz );
 
 
 export default router;
