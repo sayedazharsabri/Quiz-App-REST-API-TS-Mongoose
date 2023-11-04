75632b03fbbe32d1030f7485db1f6b71c3b0d972
manish50kumar <manish5060773@gmail.com>
2023-11-04 22:01:28 +0530

    send email for deactivate account

--- backend/src/controllers/user.ts
+++ backend/src/controllers/user.ts
@@ -5,6 +5,9 @@ import ProjectError from "../helper/error";
 import User from "../models/user";
 import { ReturnResponse } from "../utils/interfaces";
 
+import sendEmail from "../utils/email";
+import jwt from "jsonwebtoken";
+
 const getUser: RequestHandler = async (req, res, next) => {
   let resp: ReturnResponse;
 
@@ -138,15 +141,34 @@ const deactivateUser: RequestHandler = async (req, res, next) => {
       throw err;
     }
 
-    user.isDeactivated = true;
-    await user.save();
+    // Email verification when User wants to deactivated account
+    const secretKey = process.env.SECRET_KEY || "";
+    const emailToken = jwt.sign({ userId: user._id }, secretKey, {
+      expiresIn: "5m",
+    });
 
-    resp = { status: "success", message: "User deactivated!", data: {} };
+    const message = `
+    Click on the below link to deactivate your account:
+    http://${process.env.BASE_URL}/user/deactivate/${emailToken}
+    
+    (Note: If the link is not clickable kindly copy the link and paste it in the browser.)`;
+    sendEmail(user.email, "Verify Email", message);
+    resp = {
+      status: "success",
+      message: "An Email has been sent to your account please verify!",
+      data: {},
+    };
+
+    // user.isDeactivated = true;
+    // await user.save();
+
+    // resp = { status: "success", message: "User deactivated!", data: {} };
     res.status(200).send(resp);
   } catch (error) {
     next(error);
   }
 };
+
 
 const isActiveUser = async (userId: String) => {
   const user = await User.findById(userId);
