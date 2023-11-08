1ea3698a6121e7286179aa6bbcf43ccadf320709
Shiwang0-0 <bisht0shiwang@gmail.com>
2023-11-08 18:48:16 +0530

    moved favourite questions functions to a seperate controller file

--- backend/src/controllers/user.ts
+++ backend/src/controllers/user.ts
@@ -4,7 +4,6 @@ import bcrypt from "bcryptjs";
 import ProjectError from "../helper/error";
 import User from "../models/user";
 import { ReturnResponse } from "../utils/interfaces";
-import favQuestion from "../models/favQuestion";
 import sendEmail from "../utils/email";
 import jwt from "jsonwebtoken";
 
@@ -211,62 +210,6 @@ const deactivateUserCallback: RequestHandler = async (req, res, next) => {
   }
 }
 
-const addFavQuestion: RequestHandler = async (req, res, next) => {
-  let resp: ReturnResponse;
-  const userId = req.userId;
-  const options = req.body.options;
-  const question = req.body.question;
-
-  try {
-    const user = await User.findById(userId);
-    if (!user) {
-      const err = new ProjectError("User does not exist");
-      err.statusCode = 401;
-      throw err;
-    }
-    
-    const favQues = new favQuestion({ question, options,userId });    
-    await favQues.save();
-    resp = { status: "success", message: "Question added to Favourites!", data: {} };
-    res.status(200).send(resp);
-  }
-  catch (error) {
-    next(error);
-  }
-};
-
-const showFavQuestion: RequestHandler = async (req, res, next) => {
-  const userId = req.userId;
-  let resp: ReturnResponse;
-  try {
-    const favQues = await favQuestion.find({userId}); 
-      resp = { status: "success", message: "Favourite Questions!", data: {favQues} };
-      res.status(200).send(resp);
-  } 
-  catch (error) {
-    next(error);
-  }
-}
-
-//user will get favourites only when he is authenticated,and once he get the id from fav collection he can delete it.
-
-const removeFavQuestion: RequestHandler = async (req, res, next) => {
-
-  const questionId = req.params.favquestionId;
-  try {
-    await favQuestion.deleteOne({_id:questionId});
-    const resp: ReturnResponse = {
-      status: "success",
-      message: "Question removed from favourites successfully",
-      data: {},
-    };
-    res.status(200).send(resp);
-  }
-  catch (error) {
-    next(error);
-  }
-}
-
 const isActiveUser = async (userId: String) => {
   const user = await User.findById(userId);
 
@@ -278,4 +221,4 @@ const isActiveUser = async (userId: String) => {
   return !user.isDeactivated;
 };
 
-export { deactivateUser, getUser, isActiveUser, updateUser, changePassword, deactivateUserCallback, addFavQuestion, showFavQuestion, removeFavQuestion};
+export { deactivateUser, getUser, isActiveUser, updateUser, changePassword, deactivateUserCallback};
