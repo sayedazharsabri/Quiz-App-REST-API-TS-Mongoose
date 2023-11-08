15167885127f0af33de6f9c4820d31e8710274f2
Shiwang0-0 <bisht0shiwang@gmail.com>
2023-11-08 18:20:44 +0530

    Favourite Question model file name changed

---
+++ backend/src/models/favQuestion.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const schema = mongoose.Schema;
+//schema
+const favSchema = new schema(
+    {
+        userId:{
+            type:mongoose.Types.ObjectId
+        },
+        question: {
+            type: String,
+            required:true
+        },
+        options:{}
+    },
+);
+
+const favQuestion = mongoose.model("FavouriteQuestion", favSchema);
+
+export default favQuestion;
